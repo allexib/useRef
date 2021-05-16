0ae0e50ef9f2166f06e3c3d1bf39de2dc5714e52
allexib <ooomegasib@gmail.com>
2021-05-16 18:17:34 +0400

    useRef how to work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
 
+
 function App() {
-  const [numbers, setNumbers] = React.useState([1, 2, 3, 4, 5]);
+    let ulElem;
+    const [numbers, setNumbers] = React.useState([1, 2, 3, 4, 5, 6, 7]);
+    const ulRef = React.useRef();
 
-  const addNumber = () => {
-    const lastNumber = numbers[numbers.length - 1];
-    setNumbers([...numbers, lastNumber + 1]);
-  };
+    console.log(ulRef);
 
-  return (
-      <div>
-        <ul>
-            {numbers.map((n) => (
-              <li key={n}>{n}</li>
-            ))}
-        </ul>
-        <button onClick={addNumber}> Add number</button>
-      </div>
-  )
+    const addNumber = () => {
+        const lastNumber = numbers[numbers.length - 1];
+        setNumbers([...numbers, lastNumber + 1]);
+    };
+
+    const handleScroll = () => {
+        console.log('was scroll');
+    };
+
+    React.useEffect(() => {
+        console.log(ulRef);
+    }, []);
+
+
+    const removeScroll = () => {
+        ///ulElem.removeEventListener('scroll', handleScroll);
+    };
+
+    return (
+        <div>
+            <ul ref={ulRef}>
+                {numbers.map((n) => (
+                    <li key={n}>{n}</li>
+                ))}
+            </ul>
+            <button onClick={addNumber}> Add number</button>
+            <button onClick={removeScroll}>stop watching</button>
+        </div>
+    );
 }
 
 export default App;
